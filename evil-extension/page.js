aa78efda2d3ef8d39b4e05d4e5dfb70fdde75d61
MeteorV <ekids12345@gmail.com>
2019-04-26 16:03:22 +0800

    ohhhh finally !!

--- evil-extension/page.js
+++ evil-extension/page.js
@@ -10,9 +10,40 @@ const MATCH_LIST = {
   'THEY\'RE': 'THERE'
 };
 
+
 function transformTextNodes(node) {
   // TODO(you): Implement this function! See HW spec for details.
+
+  var nextNode = function (node, deeperDone) {
+    return !node ? undefined
+      : node.childNodes.length && !deeperDone ? node.childNodes[0]
+        : node.nextSibling ? node.nextSibling
+          : nextNode(node.parentNode, 1);
+  };
+
+  for (var node = document.body; node; node = nextNode(node)) {
+    console.log(node.nodeName);
+    if (node.nodeName == "BODY" || node.nodeName == "SCRIPT" || node.nodeName == "STYLE" || node.nodeName == "svg" || node.nodeName == "HEADER" || node.nodeName == "#text" || node.nodeName == "COMMENT" || node.nodeName == "DIV" || node.nodeName == "MAIN" )
+      continue;
+    console.log("Start to detect : " + node.innerHTML);
+    var words = node.innerHTML.split(/[ \n]/);
+    console.log(words);
+    var recovery_text = "";
+    
+    for(var i=0; i < words.length; i++ ){
+      if (words[i] in MATCH_LIST ){
+        console.log("before : " + words[i] + " match : " + MATCH_LIST[words[i]]);
+        words[i] = MATCH_LIST[words[i]];
+      }
+      recovery_text += words[i] + " " ;
+    }
+    //console.log(recovery_text);
+    node.innerHTML = recovery_text;
+    //if (node.id) console.log(node.id); // skip text nodes
+  }
 }
+
+
 
 transformTextNodes(document.body);
 
